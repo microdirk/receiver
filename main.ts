86ba84d9d08fc328682f294cbb964fd8963fd5e1
x-functions <84573090+xfunctions@users.noreply.github.com>
2021-11-23 11:48:12 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 radio.onReceivedValue(function (name, value) {
-    bitbot.oledNumber(value, 0, 0, false)
-    bitbot.oledText(name, 0, 20, false)
     if (name == "Left") {
+        bitbot.oledText(name, 0, 0, false)
+        bitbot.oledNumber(value, 50, 0, false)
         if (value == 1) {
             led.plot(0, 4)
             led.plot(0, 3)
@@ -11,6 +11,8 @@ radio.onReceivedValue(function (name, value) {
         }
     }
     if (name == "Center") {
+        bitbot.oledText(name, 0, 1, false)
+        bitbot.oledNumber(value, 50, 1, false)
         if (value == 1) {
             led.plot(2, 4)
             led.plot(2, 3)
@@ -19,7 +21,11 @@ radio.onReceivedValue(function (name, value) {
             led.plot(2, 0)
         }
     }
+    basic.pause(100)
+    basic.clearScreen()
     if (name == "Right") {
+        bitbot.oledText(name, 0, 2, false)
+        bitbot.oledNumber(value, 50, 2, false)
         if (value == 1) {
             led.plot(4, 4)
             led.plot(4, 3)
@@ -28,7 +34,6 @@ radio.onReceivedValue(function (name, value) {
             led.plot(4, 0)
         }
     }
-    basic.pause(100)
-    basic.clearScreen()
 })
+bitbot.oledText("Hello!", 0, 0, false)
 radio.setGroup(1)
